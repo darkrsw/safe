142e0f9b98a198344c54601a0289398c2019d45d
Dongsun Kim <darkrsw@gmail.com>
2016-11-19 19:57:43 +0100

    cleanup.

--- scripts/collect.sh
+++ scripts/collect.sh
@@ -5,6 +5,6 @@ OUTPATH="/mnt/exp2/data/js/output/repo-pack-$1"
 
 CLASSPATH="../target/scala-2.9.2/classes:../target/pack/lib/*"
 
-java -cp "$CLASSPATH" -Xmx32g edu.lu.uni.serval.js.exp.safe.AlarmCollectorByPack $REPOPATH $OUTPATH $CLASSPATH
+java -cp "$CLASSPATH" -Xmx32g edu.lu.uni.serval.js.exp.safe.AlarmCollectorByPack $REPOPATH $OUTPATH "$CLASSPATH"
 
 echo "SAFE alarm collection for repo-pack-$1" | mail -s "alarm collection completed $1" darkrsw@gmail.com
